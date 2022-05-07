4db6bdb47b68e4fd3f12c0ba519d965013a84905
Ratmir Gaitov <gaitovrat@gmail.com>
2022-05-07 18:49:40 +0200

    Geberate java project using maven

---
+++ src/test/java/vsb/gai0010/AppTest.java
@@ -0,0 +1,12 @@
+package vsb.gai0010;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
