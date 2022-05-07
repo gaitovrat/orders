9a66b45bd6f457bfc273c0da2b5de98c4a89a234
Ratmir Gaitov <gaitovrat@gmail.com>
2022-05-08 00:41:44 +0200

    Create frontend templates

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./dist/**/*.{html,js}'],
+    theme: {
+        extend: {
+            gridTemplateRows: {
+                '[auto,auto,1fr]': 'auto auto 1fr',
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio')
+    ],
+}
